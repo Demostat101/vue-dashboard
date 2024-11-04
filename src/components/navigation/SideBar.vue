24939b17543c79423bbe0880dddb1a5a9ed609cb
Demostat101 <demostat1995@gmail.com>
2024-11-05 00:45:11 +0100

    add animation

--- src/components/navigation/SideBar.vue
+++ src/components/navigation/SideBar.vue
@@ -32,17 +32,17 @@ const handleClick = () => {
     :class="!props.toggle ?` max-w-[320px] w-full sm:fixed 2xl:static sm:translate-x-[-150%] 2xl:transform-none transition-all duration-1000 2xl:block h-screen bg-[#242290] pl-5 pt-[40px] sm:z-20` :'max-w-[320px] w-full sm:fixed 2xl:static sm:translate-x-[0%] 2xl:transform-none   2xl:block h-screen bg-[#242290] pl-5 pt-[40px] transition-all duration-1000 sm:z-20'"
   >
     <div class="flex mb-[40px] gap-5">
-      <component :is="jt"/> <span class="font-[700] text-[32px] leading-[48px] text-white">JODNA TECH</span>
+      <component :is="jt"/> <span class="font-[700] text-[32px] leading-[48px] text-white bouncename-animation">JODNA TECH</span>
     </div>
     <div>
-      <div v-for="item in menuItems" :key="item.id" class="w-full mb-2 flex flex-col gap-2">
+      <div v-for="item in menuItems" :key="item.id" class="w-full mb-2 flex flex-col gap-2 parent-container">
         <router-link :to="item.href"  :class="
-                route.path === item.href ? 'bg-white text-[#242290] px-6 py-2 w-full flex place-items-center gap-6 rounded-s-full' : 'bg-[#242290] flex place-items-center px-6 py-2 gap-6'
+                route.path === item.href ? 'bg-white text-[#242290] px-6 py-2 w-full flex place-items-center gap-6 rounded-s-full parent-container' : 'bg-[#242290] flex place-items-center px-6 py-2 gap-6 parent-container'
               ">
             <component
               :is="item.icon"
               :class="
-                route.path !== item.href ? 'text-[#C1BBEB] ' : 'text-[#4D44B5]'
+                route.path !== item.href ? 'text-[#C1BBEB] bounce-animation ' : 'text-[#4D44B5] bounce-animation'
               "
 
             />
@@ -62,8 +62,15 @@ const handleClick = () => {
   </div>
 </template>
 
-<style>
-  .hide{
-    transform: translateX(0%);
-  }
+<style scoped>
+
+
+.bouncename-animation {
+  animation: bounce 3s infinite;
+}
+
+.parent-container:hover .bounce-animation {
+  animation: bounce 1s infinite;
+}
+
 </style>
